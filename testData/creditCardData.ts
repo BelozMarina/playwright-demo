c1a764858e794627f92780150564abbd755ad9fc
marina <m.bielozorova@treeum.net>
2025-05-04 01:38:45 +0300

    debug test

--- testData/creditCardData.ts
+++ testData/creditCardData.ts
@@ -1,8 +1,9 @@
+import { envConfig } from '../env.config';
 import { ICreditCard } from '../typings/ICreditCard';
 
 export const creditCard: ICreditCard = {
-  cardNumber: process.env.CARD_NUMBER!,
-  cardHolderName: process.env.CARD_HOLDER!,
-  expiryDate: process.env.CARD_EXPIRY!,
-  cvv: process.env.CARD_CVV!,
+  cardNumber: envConfig.CARD_NUMBER,
+  cardHolderName: envConfig.CARD_HOLDER,
+  expiryDate: envConfig.CARD_EXPIRY_DATE,
+  cvv: envConfig.CARD_CVV,
 };
