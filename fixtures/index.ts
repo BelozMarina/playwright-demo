5704af31c165b97b5f486c8da7f79b4d32bb6b4f
marina <m.bielozorova@treeum.net>
2025-05-07 21:55:53 +0300

    debug test on CI

--- fixtures/index.ts
+++ fixtures/index.ts
@@ -2,7 +2,7 @@ import path from 'path';
 import { envConfig } from '../env.config';
 import { AppPage } from '../pages/appPage';
 import { test as baseTest } from '@playwright/test';
-import fs from 'fs';
+// import fs from 'fs';
 
 type MyFixtures = {
   app: AppPage;
@@ -17,27 +17,27 @@ export const test = baseTest.extend<MyFixtures>({
   loggedApp: async ({ browser }, use) => {
     const userAuthFile = path.join(__dirname, '../.auth/user.json');
     // const isCI = process.env.CI === 'true';
-    let context;
+    // let context;
 
-    if (!fs.existsSync(userAuthFile)) {
-      // If no auth file, create new context and perform login
-      context = await browser.newContext();
-      const page = await context.newPage();
-      const app = new AppPage(page);
-
-      await app.loginPage.goto();
-      await app.loginPage.headerFragment.navigateToLoginPage();
-      await app.loginPage.login(envConfig.USER_EMAIL, envConfig.USER_PASSWORD);
-      await app.page.context().storageState({ path: userAuthFile });
-    } else {
-      // If auth file exists, use it
-      context = await browser.newContext({
-        storageState: userAuthFile,
-      });
-    }
-
+    // if (!fs.existsSync(userAuthFile)) {
+    // If no auth file, create new context and perform login
+    const context = await browser.newContext();
     const page = await context.newPage();
     const app = new AppPage(page);
+
+    await app.loginPage.goto();
+    await app.loginPage.headerFragment.navigateToLoginPage();
+    await app.loginPage.login(envConfig.USER_EMAIL, envConfig.USER_PASSWORD);
+    await app.page.context().storageState({ path: userAuthFile });
+    // } else {
+    //   // If auth file exists, use it
+    //   context = await browser.newContext({
+    //     storageState: userAuthFile,
+    //   });
+    // }
+
+    // const page = await context.newPage();
+    // const app = new AppPage(page);
     await use(app);
     await context.close();
   },
