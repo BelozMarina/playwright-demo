5f657dd346e3f8fae02469baa12dd9d6078cd8c8
marina <m.bielozorova@treeum.net>
2025-05-18 22:39:30 +0300

    added tags

--- tests/filterCategory.spec.ts
+++ tests/filterCategory.spec.ts
@@ -1,41 +1,51 @@
 import { Categories, PowerTools } from '../pages/home/enums/categoryEnum';
 import { test } from '../fixtures';
 
-test('Verify user can filter products by category @Ta97cca06', async ({ app }) => {
-  await test.step('Navigate to home page', async () => {
-    await app.homePage.goto();
-  });
+test(
+  'Verify user can filter products by category @Ta97cca06',
+  { tag: '@regression' },
+  async ({ app }) => {
+    await test.step('Navigate to home page', async () => {
+      await app.homePage.goto();
+    });
 
-  await test.step(`Select option sorting by ${PowerTools.SANDER}`, async () => {
-    await app.homePage.filtersFragment.selectSubCategory(PowerTools.SANDER);
-  });
+    await test.step(`Select option sorting by ${PowerTools.SANDER}`, async () => {
+      await app.homePage.filtersFragment.selectSubCategory(PowerTools.SANDER);
+    });
 
-  await test.step(`Verify products are filtered by ${PowerTools.SANDER}`, async () => {
-    await app.homePage.expectFilteredCategoryBySelectedName(PowerTools.SANDER);
-  });
-});
+    await test.step(`Verify products are filtered by ${PowerTools.SANDER}`, async () => {
+      await app.homePage.expectFilteredCategoryBySelectedName(
+        PowerTools.SANDER
+      );
+    });
+  }
+);
 
 [
   [Categories.OTHER, Categories.POWER_TOOLS],
   [Categories.OTHER, Categories.HAND_TOOLS],
 ].forEach((categories) => {
-  test(`Verify user can filter products by several categories - ${categories.join(
-    ' and '
-  )} and all sub-categories @T585f2a3a`, async ({ app }) => {
-    let countPage = 0;
+  test(
+    `Verify user can filter products by several categories - ${categories.join(
+      ' and '
+    )} and all sub-categories @T585f2a3a`,
+    { tag: '@regression' },
+    async ({ app }) => {
+      let countPage = 0;
 
-    await test.step('Navigate to home page', async () => {
-      await app.homePage.goto();
-    });
+      await test.step('Navigate to home page', async () => {
+        await app.homePage.goto();
+      });
 
-    await test.step(`Select option sorting by Categories`, async () => {
-      countPage = await app.homePage.filtersFragment.selectCustomCategories(
-        categories
-      );
-    });
+      await test.step(`Select option sorting by Categories`, async () => {
+        countPage = await app.homePage.filtersFragment.selectCustomCategories(
+          categories
+        );
+      });
 
-    await test.step(`Verify products are filtered by several category`, async () => {
-      await app.homePage.expectCountPagesBySelectedCategories(countPage);
-    });
-  });
+      await test.step(`Verify products are filtered by several category`, async () => {
+        await app.homePage.expectCountPagesBySelectedCategories(countPage);
+      });
+    }
+  );
 });
--- tests/sortProducts.spec.ts
+++ tests/sortProducts.spec.ts
@@ -7,41 +7,45 @@ import {
 import { SortOption } from '../pages/home/fragments/products.filters.fragment';
 
 sortOptionNameData.forEach((sortOpt) => {
-  test(`Verify user can perform sorting products by ${sortOpt} @Tf7883a58`, async ({
-    page,
-  }) => {
-    const homePage = new HomePage(page);
+  test(
+    `Verify user can perform sorting products by ${sortOpt} @Tf7883a58`,
+    { tag: '@regression' },
+    async ({ page }) => {
+      const homePage = new HomePage(page);
 
-    await test.step('Navigate to home page', async () => {
-      await homePage.goto();
-    });
+      await test.step('Navigate to home page', async () => {
+        await homePage.goto();
+      });
 
-    await test.step(`Select option sorting by ${sortOpt}`, async () => {
-      await homePage.filtersFragment.selectSortOptions(sortOpt as SortOption);
-    });
+      await test.step(`Select option sorting by ${sortOpt}`, async () => {
+        await homePage.filtersFragment.selectSortOptions(sortOpt as SortOption);
+      });
 
-    await test.step(`Verify products are sorted by ${sortOpt}`, async () => {
-      await homePage.expectSortedProductsByName(sortOpt as SortOption);
-    });
-  });
+      await test.step(`Verify products are sorted by ${sortOpt}`, async () => {
+        await homePage.expectSortedProductsByName(sortOpt as SortOption);
+      });
+    }
+  );
 });
 
 sortOptionPriceData.forEach((option) => {
-  test(`Verify user can perform sorting products by ${option} @T8359a3a0`, async ({
-    page,
-  }) => {
-    const homePage = new HomePage(page);
+  test(
+    `Verify user can perform sorting products by ${option} @T8359a3a0`,
+    { tag: '@regression' },
+    async ({ page }) => {
+      const homePage = new HomePage(page);
 
-    await test.step('Navigate to home page', async () => {
-      await homePage.goto();
-    });
+      await test.step('Navigate to home page', async () => {
+        await homePage.goto();
+      });
 
-    await test.step(`Select option sorting by ${option}`, async () => {
-      await homePage.filtersFragment.selectSortOptions(option as SortOption);
-    });
+      await test.step(`Select option sorting by ${option}`, async () => {
+        await homePage.filtersFragment.selectSortOptions(option as SortOption);
+      });
 
-    await test.step(`Verify products are sorted by ${option}`, async () => {
-      await homePage.expectSortedProductsByPrice(option as SortOption);
-    });
-  });
+      await test.step(`Verify products are sorted by ${option}`, async () => {
+        await homePage.expectSortedProductsByPrice(option as SortOption);
+      });
+    }
+  );
 });
